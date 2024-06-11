bc5124661f6e9c4f25f37ec354b98d0edeecec03
Johannes Mogashoa <jomogashoa1993@gmail.com>
2024-06-11 23:07:03 +0200

    Initial Supabase config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/eslint"],
+  modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxtjs/supabase"],
+  css: ["@/assets/css/main.css"],
+  supabase: {
+    redirectOptions: {
+      login: "/login",
+      callback: "/confirm",
+      include: undefined,
+      exclude: [],
+      cookieRedirect: true,
+    },
+  },
 });
